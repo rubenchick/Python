01283de38705b32a10646fae16a92ebc26cef612
Anthony Ruby <anthony.ruby@kiv.dev>
2023-04-09 18:59:57 +0300

    Bow and Cow

--- Tasks/0299 Bulls and Cows.py
+++ Tasks/0299 Bulls and Cows.py
@@ -6,42 +6,75 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     secret = "1807"
-    guess = "7810"
-    secret = "1123"
-    guess = "0111"
+    guess =  "7810"
+    # secret = "1123"
+    # guess =  "0111"
+    secret = "9845119"
+    guess =  "7811651"
+    secret = "1"
+    guess = "0"
     secret = list(secret)
     guess = list(guess)
     bow = 0
     cow = 0
     hashmap = {}
-    # for i in range(len(guess)):
     flag = False
     i = 0
     while flag == False:
-        if guess[i] == secret[i]:
-            bow += 1
-            guess.pop(i)
-            secret.pop(i)
-            i -= 1
-
-        if i == len(secret)-1:
-            flag = True
-        i += 1
-
-    flag = False
-    i = 0
-    # print(secret.index('7'))
-    while flag == False:
+        print(i)
         if guess[i] in secret:
-            cow += 1
-            secret.pop(secret.index(guess[i]))
-            guess.pop(i)
-            i -= 1
+            if guess[i] == secret[i]:
+                bow += 1
+            else:
+                if secret.count(guess[i]) == guess.count(guess[i]):
+                    cow += 1
+                else:
+                    if secret.count(guess[i]) < guess.count(guess[i]):
+                        guess[i] = "X"
+                    else:
+                        cow += 1
 
-        if i == len(secret) - 1:
+        if i == len(guess)-1:
             flag = True
+
         i += 1
-    rrr = "{}A{}B".format(bow, cow)
-    print(rrr)
+
     print("Bow :", bow)
     print("Cow :", cow)
+
+
+    # Version I. Work but late. Only 17% beats
+    # secret = list(secret)
+    # guess = list(guess)
+    # bow = 0
+    # cow = 0
+    # # hashmap = {}
+    # flag = False
+    # i = 0
+    # while flag == False:
+    #     if guess[i] == secret[i]:
+    #         bow += 1
+    #         guess.pop(i)
+    #         secret.pop(i)
+    #         i -= 1
+    #
+    #     if i == len(secret)-1:
+    #         flag = True
+    #     i += 1
+    #
+    # flag = False
+    # i = 0
+    # while flag == False:
+    #     if guess[i] in secret:
+    #         cow += 1
+    #         secret.pop(secret.index(guess[i]))
+    #         guess.pop(i)
+    #         i -= 1
+    #
+    #     if i == len(secret) - 1:
+    #         flag = True
+    #     i += 1
+    # rrr = "{}A{}B".format(bow, cow)
+    # print(rrr)
+    # print("Bow :", bow)
+    # print("Cow :", cow)
