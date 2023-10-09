b39a5518a5357aa1ddbb6217013396626f9b16d0
rubenchick <36621128+rubenchick@users.noreply.github.com>
2023-10-09 16:48:48 +0300

    |

--- Basic/upload SSH.py
+++ Basic/upload SSH.py
@@ -5,37 +5,37 @@ import paramiko
 sftp_host = "ansftp.infra.travel-dev.com"
 sftp_port = 22
 sftp_user = "anthony.ruby"
-private_key_path = "/Users/rubenchik/GDrive/Languages/Python/Kivork/Improve skills/Basic/Key/sftp.txt"
+private_key_path = "/Users/rubenchik/GDrive2/Languages/Python/Kivork/Improve skills/Basic/Key/sftp.key"
 
 # Подключаемся к серверу SFTP
 private_key = paramiko.RSAKey(filename=private_key_path)
 transport = paramiko.Transport((sftp_host, sftp_port))
 transport.connect(username=sftp_user, pkey=private_key)
-
+#
 sftp = paramiko.SFTPClient.from_transport(transport)
 #
-# # Передаем файл
-local_path = "/Users/rubenchik/GDrive/Languages/Python/Kivork/Improve skills/Basic/df.py"
-remote_path = "df.py"
-sftp.put(local_path, remote_path)
+# # # Передаем файл
+# local_path = "/Users/rubenchik/GDrive/Languages/Python/Kivork/Improve skills/Basic/df.py"
+# remote_path = "df.py"
+# sftp.put(local_path, remote_path)
 
 # Проверяем наличие файла на сервере
 
 files_on_server = sftp.listdir("")
 print(files_on_server)
 
+# # # удаление файлов
+# sftp.remove(f"agent_schedule_02102023.csv")
+# files_on_server = sftp.listdir("")
+# print(files_on_server)
 
 # Загружаем файл с сервера
-remote_path = "agent_schedule.csv"
-local_path = "/Users/rubenchik/GDrive/Languages/Python/Kivork/Improve skills/Basic/agent_schedule.csv"
+remote_path = "agent_schedule_05102023.csv"
+local_path = "/Users/rubenchik/GDrive2/Languages/Python/Kivork/Improve skills/Basic/agent_schedule_05102023.csv"
 sftp.get(remote_path, local_path)
 
 print(f"Файл {remote_path} успешно загружен в {local_path}")
 
-# Закрываем соединение
-sftp.close()
-transport.close()
-
-# Закрываем соединение
+# # Закрываем соединение
 sftp.close()
 transport.close()
--- Tasks/0377. Combination Sum IV.py
+++ Tasks/0377. Combination Sum IV.py
@@ -5,20 +5,55 @@
 # The possible combination ways are:
 # (1, 1, 1, 1),   (1, 1, 2), (1, 2, 1), (1, 3), (2, 1, 1), (2, 2), (3, 1)
 
-nums = [1, 2, 3, 9]
-target = 4
+nums = [10, 20, 30, 90]
+target = 40
 
 class Solution(object):
     def combinationSum4(self, nums, target):
         # remove digit more than target
         filtered = filter(lambda item: item < target, nums)
         new_nums = list(filtered)
-        def reqursion(list_, target_):
-            return list_
+        def reqursion(idx, current_list, pos):
+            print(pos, len(new_nums))
+            while pos < len(new_nums):
+                print(pos)
+                reqursion(idx, current_list, pos + 1)
 
-        # reqursion(new_nums, target)
+            return
 
-        return reqursion(new_nums, target)
+            #
+            #
+            # current_list.append(new_nums[pos])
+            # if sum(current_list)> target:
+            #     return
+            # else:
+            #     if sum(current_list) == target:
+            #         print(current_list)
+            #         return
+            #     else:
+            #         reqursion(idx, current_list, pos)
+            #         # reqursion(idx, current_list, pos+1)
+
+
+
+
+
+            # current_list.append(new_nums[pos])
+            # if sum(current_list) > target:
+            #     return
+            # else:
+            #     if sum(current_list) == target:
+            #         print(current_list)
+            #         return
+            #     else:
+            #         reqursion(idx, current_list, pos+1)
+
+        for idx in range(len(new_nums)):
+            current_list = [new_nums[idx]]
+            pos = 0
+            reqursion(idx, current_list, pos)
+
+        return 99
 
 if __name__ == '__main__':
     result = Solution()
